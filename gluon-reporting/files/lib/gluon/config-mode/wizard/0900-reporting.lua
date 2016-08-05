7556e8517b27af513874e794ed9d48c6eafc5d39
aktienmakler <github@drobny.it>
2016-08-05 11:34:45 +0200

    Update 0900-reporting.lua

--- gluon-reporting/files/lib/gluon/config-mode/wizard/0900-reporting.lua
+++ gluon-reporting/files/lib/gluon/config-mode/wizard/0900-reporting.lua
@@ -4,59 +4,41 @@ local uci = luci.model.uci.cursor()
 local M = {}
 
 function M.section(form)
-        local s = form:section(cbi.SimpleSection, nil, [[Stellen Sie die Benachrichtigungen ein, die vom Monitoring-System an Ihre Kontaktadresse versandt werden.]])
+	local s = form:section(cbi.SimpleSection, nil, [[Stellen Sie die Benachrichtigungen ein, die vom Monitoring-System an Ihre Kontaktadresse versandt werden.]])
 
-        local o
-          o = s:option(cbi.Flag, "_use_reporting", "Reports erhalten")
-        o.default = uci:get_first("gluon-reporting", "client", "use_reporting", o.disabled)
-        o.rmempty = false
+	local o
+	o = s:option(cbi.Flag, "_use_reporting", "Reports erhalten")
+	o.default = uci:get_first("gluon-reporting", "reporting", "use_reporting", o.disabled)
+	o.rmempty = false
 
-        o = s:option(cbi.Value, "_nodeName", "nodeName")
-        o.value = uci:get_first("gluon-provisioning", "client", "nodeName")
-        o.rmempty = false
-        
-        
-local o = s:option(ListValue, "frequency", translate("Report frequency"))
-o.default = ""
-o:value("", translate("Chose frequency"))
-o:value("d", translate("daily"))
-o:value("w", translate("weekly"))
-o:value("m", translate("monthly"))
-o:depends("_use_reporting", "1")
+	o = s:option(ListValue, "_report_frequency", translate("Report frequency"))
+	o.default = uci:get_first("gluon-reporting", "reporting", "report_frequency", "m")
+	o.value("", translate("Chose frequency"))
+	o.value("d", translate("daily"))
+	o.value("w", translate("weekly"))
+	o.value("m", translate("monthly"))
+	o.depends("_use_reporting", "1")
+	o.widget = "select"
 
-        o = s:option(cbi.Value, "_nodePassword", "nodePassword")
-        o.value = uci:get_first("gluon-provisioning", "client", "nodePassword")
-        o.rmempty = false
-        o:depends("_use_provisioning", "1")
+	o = s:option(cbi.Flag, "_report_error", translate("Report error"))
+	o.default = uci:get_first("gluon-reporting", "reporting", "report_error", 1)
+	o.depends("_use_reporting", "1")
 
-        o = s:option(cbi.Value, "_uuid", "UUID")
-        o.value = uci:get_first("gluon-provisioning", "client", "UUID")
+	o = s:option(cbi.Flag, "_report_recovery", translate("Report recovery"))
+	o.default = uci:get_first("gluon-reporting", "reporting", "report_recovery", 0)
+	o.depends("_use_reporting", "1")
 
 end
 
 function M.handle(data)
-        local sname = uci:get_first("gluon-provisioning", "client")
-        uci:set("gluon-provisioning", sname, "use_provisioning", data._use_provisioning)
+        local sname = uci:get_first("gluon-reporting", "report")
+        uci:set("gluon-reporting", sname, "use_reporting", data._use_reporting)
+        uci:set("gluon-reporting", sname, "report_frequency", data._report_frequency)
+        uci:set("gluon-reporting", sname, "report_error", data._report_error)
+        uci:set("gluon-reporting", sname, "report_recovery", data._report_recovery)
 
-        local uuid = uci:get_first("gluon-provisioning", "client", "UUID")
-        if uuid == nil then
-                f = assert(io.popen("cat /proc/sys/kernel/random/uuid"))
-                local result
-                for line in f:lines() do
-                        result = line
-                end
-                uci:set("gluon-provisioning", sname, "UUID", result)
-        end
-        if data._use_provisioning and data._nodeName ~= nil and data._nodePassword ~= nil then
-                uci:set("gluon-provisioning", sname, "nodeName", data._nodeName)
-                uci:set("gluon-provisioning", sname, "nodePassword", data._nodePassword)
-        else
-                uci:delete("gluon-provisioning", sname, "nodeName")
-                uci:delete("gluon-provisioning", sname, "nodePassword")
-        end
-
-        uci:save("gluon-provisioning")
-        uci:commit("gluon-provisioning")
+        uci:save("gluon-reporting")
+        uci:commit("gluon-reporting")
 end
 
 return M
